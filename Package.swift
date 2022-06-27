a67d0e10ad7efb4906c79b08ea109b0c40bb4628
Jiseong <yim2627@gmail.com>
2022-06-27 21:29:08 +0900

    chore: revise package description

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "StackyText",
+    platforms: [.iOS(.v14)],
+    swiftLanguageVersions: [.v4],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
